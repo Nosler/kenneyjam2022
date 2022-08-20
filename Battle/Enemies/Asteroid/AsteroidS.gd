dd5010a9849325afbce9f1924078c1972ccc608e
Madeline <finegan.madeline@gmail.com>
2022-08-20 14:57:21 -0400

    Everything

---
+++ Battle/Enemies/Asteroid/AsteroidS.gd
@@ -0,0 +1,16 @@
+extends RigidBody2D
+
+var edge_warp_thresh = 0
+func _integrate_forces(state):
+	if position.x >= edge_warp_thresh or position.x <= -edge_warp_thresh:
+		state.transform.origin = Vector2(clamp(-position.x, -edge_warp_thresh, edge_warp_thresh), clamp(position.y, -edge_warp_thresh, edge_warp_thresh))
+	if position.y >= edge_warp_thresh or position.y <= -edge_warp_thresh:
+		state.transform.origin = Vector2(clamp(position.x, -edge_warp_thresh, edge_warp_thresh), clamp(-position.y, -edge_warp_thresh, edge_warp_thresh))
+
+func take_dmg(_n):
+	queue_free()
+
+func force_scale(size):
+	$Sprite.scale.x = range_lerp(size, 90, 1000, 0, .5)
+	$Sprite.scale.y = range_lerp(size, 90, 1000, 0, .5)
+	$CollisionShape2D.shape.radius = (size * 15 / 1000)
