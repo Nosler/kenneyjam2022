ba104fe2dd05e00b5e2e2a53af41c9c7cf9d2a2b
Andrea Nosler <andinosler@gmail.com>
2022-08-27 22:57:15 -0400

    tabs -> Spaces

--- EncounterHandler/EncounterHandler.gd
+++ EncounterHandler/EncounterHandler.gd
@@ -9,158 +9,157 @@ var playerdata = {}
 var encounterdata = {}
 var rng = RandomNumberGenerator.new()
 
-var enemies = { 
-	"lg_asteroid": {
-		"name": "lg_asteroids", 
-		"xp":2,
-		"paperclips":100,
-		"tier":1
-		},
-	"sm_asteroid": {
-		"name": "sm_asteroids", 
-		"xp":1,
-		"paperclips":50,
-		"paperclips":50,
-		"tier":1
-		},
-	"turret": {
-		"name": "turrets", 
-		"xp":4,
-		"paperclips":150,
-		"tier":2
-		},
-	"kamikaze": {
-		"name": "kamikazes", 
-		"xp":3,
-		"paperclips":200,
-		"tier":3
-		}
+var enemies = {
+    "lg_asteroid": {
+        "name": "lg_asteroids",
+        "xp":2,
+        "paperclips":100,
+        "tier":1
+        },
+    "sm_asteroid": {
+        "name": "sm_asteroids",
+        "xp":1,
+        "paperclips":50,
+        "tier":1
+        },
+    "turret": {
+        "name": "turrets",
+        "xp":4,
+        "paperclips":150,
+        "tier":2
+        },
+    "kamikaze": {
+        "name": "kamikazes",
+        "xp":3,
+        "paperclips":200,
+        "tier":3
+        }
 }
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	PlayerDataHandler = get_node("/root/PlayerDataHandler")
-	playerdata = PlayerDataHandler.PlayerData.ship
-	load_encounter_data()
-	
-	
+    PlayerDataHandler = get_node("/root/PlayerDataHandler")
+    playerdata = PlayerDataHandler.PlayerData.ship
+    load_encounter_data()
+
+
 func load_encounter_data():
-	var file = File.new()
+    var file = File.new()
 
-	if not file.file_exists(EncounterDataFilepath):
-		print("Error attempting to load Current Encounter json, filepath not found [%s])" % EncounterDataFilepath)
-		return
-		
-	file.open(EncounterDataFilepath, File.READ)
-		
-	var text = file.get_as_text()
-	var data_parse = JSON.parse(text)
+    if not file.file_exists(EncounterDataFilepath):
+        print("Error attempting to load Current Encounter json, filepath not found [%s])" % EncounterDataFilepath)
+        return
 
-	if data_parse.error != OK:
-		print("Error returned when attempting to parse json file in encounter generation")
-		return
+    file.open(EncounterDataFilepath, File.READ)
 
-	encounterdata = data_parse.result
+    var text = file.get_as_text()
+    var data_parse = JSON.parse(text)
 
-	file.close()
+    if data_parse.error != OK:
+        print("Error returned when attempting to parse json file in encounter generation")
+        return
+
+    encounterdata = data_parse.result
+
+    file.close()
 
 
 func save_encounter_data():
-	var file = File.new()
-	
-	file.open(EncounterDataFilepath, File.WRITE)
-	file.store_line(to_json(encounterdata))
-	
-	file.close()
+    var file = File.new()
+
+    file.open(EncounterDataFilepath, File.WRITE)
+    file.store_line(to_json(encounterdata))
+
+    file.close()
 
 
 func reset_encounter_data():
-	var file = File.new()
-	
-	if not file.file_exists(EmptyEncounterFilepath):
-		print("Error attempting to load Empty Encounter json, filepath not found [%s])" % EmptyEncounterFilepath)
-		return
-	
-	file.open(EmptyEncounterFilepath, File.READ)
-	
-	var text = file.get_as_text()
-	var data_parse = JSON.parse(text)
+    var file = File.new()
 
-	if data_parse.error != OK:
-		print("Error returned when attempting to parse Empty Encounter json file in encounter generation")
-		return
+    if not file.file_exists(EmptyEncounterFilepath):
+        print("Error attempting to load Empty Encounter json, filepath not found [%s])" % EmptyEncounterFilepath)
+        return
 
-	encounterdata = data_parse.result
+    file.open(EmptyEncounterFilepath, File.READ)
 
-	file.close()
-	
-	save_encounter_data()
+    var text = file.get_as_text()
+    var data_parse = JSON.parse(text)
+
+    if data_parse.error != OK:
+        print("Error returned when attempting to parse Empty Encounter json file in encounter generation")
+        return
+
+    encounterdata = data_parse.result
+
+    file.close()
+
+    save_encounter_data()
 
 
 func gen_encounter(level):
-	var xp_budget = 0
-	var xp_used = 0
-	var enc_reward = 0
-	var tier = 0
-	var boss = encounterdata.encounter.boss # Maintains boss state
-	
-	reset_encounter_data()
-	encounterdata.encounter.boss = boss
-	
-	rng.randomize()
-	
-	# Tier 1 encounters
-	if level < 4:
-		print("Tier 1 encounter")
-		tier = 1
-		xp_budget = rng.randi_range(5,10)
+    var xp_budget = 0
+    var xp_used = 0
+    var enc_reward = 0
+    var tier = 0
+    var boss = encounterdata.encounter.boss # Maintains boss state
 
-	# Tier 2 encounters
-	elif level >= 4 and level < 9:
-		print("Tier 2 encounter")
-		tier = 2
-		xp_budget = rng.randi_range(14,25)
+    reset_encounter_data()
+    encounterdata.encounter.boss = boss
 
-	# Tier 3 encounters & post-game, scales to player level
-	elif level >= 9:
-		print("Tier 3 encounter")
-		tier = 3
-		xp_budget = rng.randi_range(2*level,4*level)
+    rng.randomize()
 
-	var attempts = 0
-	while xp_used < xp_budget and attempts < 500:
-		attempts += 1
+    # Tier 1 encounters
+    if level < 4:
+        print("Tier 1 encounter")
+        tier = 1
+        xp_budget = rng.randi_range(5,10)
 
-		var new_enemy = get_random(enemies)
+    # Tier 2 encounters
+    elif level >= 4 and level < 9:
+        print("Tier 2 encounter")
+        tier = 2
+        xp_budget = rng.randi_range(14,25)
 
-		if xp_used + new_enemy.xp <= xp_budget and new_enemy.tier <= tier:
-			xp_used += new_enemy.xp
-			enc_reward += new_enemy.paperclips
-			enc_reward += new_enemy.paperclips
-			encounterdata.encounter[new_enemy.name] += 1
+    # Tier 3 encounters & post-game, scales to player level
+    elif level >= 9:
+        print("Tier 3 encounter")
+        tier = 3
+        xp_budget = rng.randi_range(2*level,4*level)
+
+    var attempts = 0
+    while xp_used < xp_budget and attempts < 500:
+        attempts += 1
+
+        var new_enemy = get_random(enemies)
+
+        if xp_used + new_enemy.xp <= xp_budget and new_enemy.tier <= tier:
+            xp_used += new_enemy.xp
+            enc_reward += new_enemy.paperclips
+            enc_reward += new_enemy.paperclips
+            encounterdata.encounter[new_enemy.name] += 1
 
 
-	encounterdata.encounter.reward_money = enc_reward
-	encounterdata.encounter.reward_xp = xp_used
+    encounterdata.encounter.reward_money = enc_reward
+    encounterdata.encounter.reward_xp = xp_used
 
-	save_encounter_data()
+    save_encounter_data()
 
 
 func get_random(dict):
-	var a = dict.keys()
-	a = a[randi() % a.size()]
-	return dict[a]
+    var a = dict.keys()
+    a = a[randi() % a.size()]
+    return dict[a]
 
 
 func _on_BossButton_pressed():
-	encounterdata.encounter.boss = true
-	print("bossbuttontest",encounterdata.encounter.boss)
-	save_encounter_data()
-	get_tree().change_scene("res://Battle/BattleSpace/BattleSpace.tscn")
+    encounterdata.encounter.boss = true
+    print("bossbuttontest",encounterdata.encounter.boss)
+    save_encounter_data()
+    get_tree().change_scene("res://Battle/BattleSpace/BattleSpace.tscn")
 
 func _on_EncounterButton_pressed():
-	print("Encounter button pressed")
-	reset_encounter_data()
-	gen_encounter(playerdata.level)
-	get_tree().change_scene("res://Battle/BattleSpace/BattleSpace.tscn")
+    print("Encounter button pressed")
+    reset_encounter_data()
+    gen_encounter(playerdata.level)
+    get_tree().change_scene("res://Battle/BattleSpace/BattleSpace.tscn")
--- MainMenu/MainMenu.gd
+++ MainMenu/MainMenu.gd
@@ -2,36 +2,36 @@ extends CanvasLayer
 
 # Sets up PlayerDataHandler and connects buttons to their functions
 func _ready():
-	$Buttons/NewGame.connect("pressed", self, "new_game")
-	$Buttons/LoadGame.connect("pressed", self, "start_game")
-	$Buttons/Credits.connect("pressed", self, "run_credits")
-	$Buttons/Exit.connect("pressed", self, "exit_game")
-	var PlayerDataHandler = get_node("/root/PlayerDataHandler")
-	PlayerDataHandler.reset_attributes()
-	
+    $Buttons/NewGame.connect("pressed", self, "new_game")
+    $Buttons/LoadGame.connect("pressed", self, "start_game")
+    $Buttons/Credits.connect("pressed", self, "run_credits")
+    $Buttons/Exit.connect("pressed", self, "exit_game")
+    var PlayerDataHandler = get_node("/root/PlayerDataHandler")
+    PlayerDataHandler.reset_attributes()
+
 func new_game():
-	var PlayerDataHandler = get_node("/root/PlayerDataHandler")
-	PlayerDataHandler.reset_attributes()
-	start_game()
-	
+    var PlayerDataHandler = get_node("/root/PlayerDataHandler")
+    PlayerDataHandler.reset_attributes()
+    start_game()
+
 func start_game():
-	var playerlevel = get_node("/root/PlayerDataHandler").PlayerData.ship.level
-	
-	# If PlayerData shows a level 1 player, sends them to the IntroCinematic. Otherwise sends them to the hub.
-	if playerlevel > 1:
-		print("Starting game at Intro Cinematic.")
-		get_tree().change_scene("res://HUB/HUB.tscn")
-		
-	else:
-		print("Starting game at HUB.")
-		get_tree().change_scene("res://Cinematics/IntroCinematic.tscn")
+    var playerlevel = get_node("/root/PlayerDataHandler").PlayerData.ship.level
+
+    # If PlayerData shows a level 1 player, sends them to the IntroCinematic. Otherwise sends them to the hub.
+    if playerlevel > 1:
+        print("Starting game at Intro Cinematic.")
+        get_tree().change_scene("res://HUB/HUB.tscn")
+
+    else:
+        print("Starting game at HUB.")
+        get_tree().change_scene("res://Cinematics/IntroCinematic.tscn")
 
 # Displays credits
 func run_credits():
-	print("Running credits.")
-	get_tree().change_scene("res://Credits/Credits.tscn")
+    print("Running credits.")
+    get_tree().change_scene("res://Credits/Credits.tscn")
 
 # Exits game
 func exit_game():
-	print("Exiting game.")
-	get_tree().quit()
+    print("Exiting game.")
+    get_tree().quit()
